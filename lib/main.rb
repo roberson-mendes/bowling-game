abf4a96c17c164d740bbcaf9ee22329a29232ff6
roberson-mendes <roberson.mendes.c@gmail.com>
2023-03-20 12:55:15 -0300

    feat: get file from any folder

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,8 @@
 require_relative 'bowling_game'
 require 'pathname'
+require 'pry-byebug'
 
-file_path = ARGV[0] ||= 'scores.txt'
-BowlingGame.new(Pathname.getwd.join(file_path)).process_game_file
+file_path = ARGV[0] ||= '/data/scores.txt'
+BowlingGame.new(
+  Pathname.getwd.join(file_path)
+).process_game_file
