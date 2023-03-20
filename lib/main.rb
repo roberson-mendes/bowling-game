4a3d833d125a4b0f122bccdeb8128f7a3d7dc975
roberson-mendes <roberson.mendes.c@gmail.com>
2023-03-20 13:44:59 -0300

    Fix(main): fix bug
    fix bug in directory call

--- lib/main.rb
+++ lib/main.rb
@@ -2,7 +2,7 @@ require_relative 'bowling_game'
 require 'pathname'
 require 'pry-byebug'
 
-file_path = ARGV[0] ||= '/data/scores.txt'
+file_path = ARGV[0] ||= Pathname.getwd.join('data').join('scores.txt')
 BowlingGame.new(
   Pathname.getwd.join(file_path)
 ).process_game_file
